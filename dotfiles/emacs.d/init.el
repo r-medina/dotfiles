31bb5760db24aa99f3a0ceb949904b5d5d741209
Ricky Medina <ricky.medina91@gmail.com>
2020-07-13 14:37:43 -0400

    gito plugin + emacs things

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -11,6 +11,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/") t)
 (package-initialize)
 
 (when (not (package-installed-p 'use-package))
@@ -23,10 +24,11 @@
     (url-insert-file-contents "https://github.com/quelpa/quelpa/raw/master/quelpa.el")
     (eval-buffer)
     (quelpa-self-upgrade)))
+
 (quelpa
  '(quelpa-use-package
-   :fetcher git
-   :url "https://github.com/quelpa/quelpa-use-package.git"))
+   :fetcher github
+   :repo "quelpa/quelpa-use-package"))
 (require 'quelpa-use-package)
 
 (require 'use-package-ensure)
@@ -56,18 +58,19 @@
  '(custom-enabled-themes (quote (Greymatters-light)))
  '(custom-safe-themes
    (quote
-    ("fb5a4e1730c349ca59a6955cb513e00ec6adb545db79fae0dbafc28af82c59ff" "323964cdaee8528eeff0b721974bb033c44a2b73d795a31ceba554f764109a20" "0da70fb7c272d8e87b9343a6f36d7f9eed520ef64beaab03478113968fbed1d9" "0aa971daea004737402f4cd0575cfa6dc2fa00206cd03c77ee2c3b5d3b5f19d6" "9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" "0a8b8aad17f3bd4af1839a49cd97048c90db98a088b1f6f6d0d2187a3c35321d" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "319bf1bab5d05e3a4c4a165efe69d27b3d975759034074f15fe61e92c7304884" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("9b02a4fd5a0fa3d0b5d326057ecbf6e4f46186c16c6c42312b3ac29f6ee35548" "fb5a4e1730c349ca59a6955cb513e00ec6adb545db79fae0dbafc28af82c59ff" "323964cdaee8528eeff0b721974bb033c44a2b73d795a31ceba554f764109a20" "0da70fb7c272d8e87b9343a6f36d7f9eed520ef64beaab03478113968fbed1d9" "0aa971daea004737402f4cd0575cfa6dc2fa00206cd03c77ee2c3b5d3b5f19d6" "9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" "0a8b8aad17f3bd4af1839a49cd97048c90db98a088b1f6f6d0d2187a3c35321d" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "319bf1bab5d05e3a4c4a165efe69d27b3d975759034074f15fe61e92c7304884" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(custom-theme-directory "~/.emacs.d/themes")
  '(display-battery-mode t)
  '(electric-pair-mode t)
  '(global-hl-line-mode t)
  '(hscroll-margin 8)
  '(hscroll-step 1)
- '(org-agenda-files '("~/notes"))
- '(org-roam-server-mode nil)
+ '(org-agenda-files (quote ("~/notes")))
+ '(org-roam-server-mode t)
  '(package-selected-packages
    (quote
-    (atomic-chrome edit-server golint gotest go-test go-rename use-package)))
+    (org-beautify-theme pullover atomic-chrome edit-server golint gotest go-test go-rename use-package)))
+ '(perfect-margin-mode t)
  '(tramp-syntax (quote simplified) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
