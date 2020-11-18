dd52776bc8984d9108db9efa37b4582792146fb5
Ricky Medina <ricky.medina91@gmail.com>
2020-11-18 15:43:24 -0500

    updating secrets

--- dotfiles/oh-my-zsh-custom/plugins/secrets/secrets.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/secrets/secrets.plugin.zsh
@@ -1,28 +1,27 @@
+function get-secret() {
+    (( $# < 2 )) && { echo "Usage: get-secret <namespace> <which_secret>"; return }
+    local namespace=$1
+    local which_secret=$2
+
+    (( ! oploggedin )) && opsignin
+
+    op get item $namespace |
+	jq -r ".details.sections[]?.fields[]? | select(.t==\"$which_secret\").v"
+}
+
 function azure-creds() {
-    export AZURE_STORAGE_ACCOUNT=$(get-gh-secret AZURE_STORAGE_ACCOUNT)
-    export AZURE_STORAGE_KEY=$(get-gh-secret AZURE_STORAGE_KEY)
+    export AZURE_STORAGE_ACCOUNT=$(get-secret gh AZURE_STORAGE_ACCOUNT)
+    export AZURE_STORAGE_KEY=$(get-secret gh AZURE_STORAGE_KEY)
 }
 
 function registry-creds() {
-    export PKGS_TOKEN=$(get-gh-secret PKGS_TOKEN)
-    export AWS_ACCESS_KEY=$(get-gh-secret AWS_ACCESS_KEY)
-    export AWS_SECRET_KEY=$(get-gh-secret AWS_SECRET_KEY)
-    export GH_API_TOKEN=$(get-gh-secret GH_API_TOKEN)
-}
-
-# getting this to work requires having an item in your 1password with the name "gh"
-function get-gh-secret() {
-    (( $# < 1 )) && { echo "Usage: get-gh-secret <which_secret>"; return }
-    local which_secret=$1
-
-    (( ! oploggedin )) && opsignin
-
-    op get item gh | jq -r ".details.sections[]?.fields[]? | select(.t==\"$which_secret\").v"
+    export PKGS_TOKEN=$(get-secret gh PKGS_TOKEN)
+    export AWS_ACCESS_KEY=$(get-secret gh AWS_ACCESS_KEY)
+    export AWS_SECRET_KEY=$(get-secret gh AWS_SECRET_KEY)
+    export GH_API_TOKEN=$(get-secret gh GH_API_TOKEN)
 }
 
 function spotify-app-creds() {
-    (( ! oploggedin )) && opsignin
-
-    export SPOTIFY_ID=$(op get item spotify | jq -r ".details.sections[]?.fields[]? | select(.t==\"id\").v")
-    export SPOTIFY_SECRET=$(op get item spotify | jq -r ".details.sections[]?.fields[]? | select(.t==\"secret\").v")
+    export SPOTIFY_ID=$(get-secret spotify id)
+    export SPOTIFY_SECRET=$(get-secret spotify secret)
 }
