4daf7bdfa55d6e51ee1b038712cc75d7feb2453a
Ricky Medina <ricky.medina91@gmail.com>
2021-01-18 14:30:35 -0500

    updateing dotfiles - themes and other things

--- dotfiles/oh-my-zsh-custom/plugins/secrets/secrets.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/secrets/secrets.plugin.zsh
@@ -1,20 +1,22 @@
+alias os='! oploggedin && opsignin'
+
 function get-secret() {
     (( $# < 2 )) && { echo "Usage: get-secret <namespace> <which_secret>"; return }
     local namespace=$1
     local which_secret=$2
-
-    (( ! oploggedin )) && opsignin
 
     op get item $namespace |
 	jq -r ".details.sections[]?.fields[]? | select(.t==\"$which_secret\").v"
 }
 
 function azure-creds() {
+    os
     export AZURE_STORAGE_ACCOUNT=$(get-secret gh AZURE_STORAGE_ACCOUNT)
     export AZURE_STORAGE_KEY=$(get-secret gh AZURE_STORAGE_KEY)
 }
 
 function registry-creds() {
+    os
     export PKGS_TOKEN=$(get-secret gh PKGS_TOKEN)
     export AWS_ACCESS_KEY=$(get-secret gh AWS_ACCESS_KEY)
     export AWS_SECRET_KEY=$(get-secret gh AWS_SECRET_KEY)
@@ -22,6 +24,7 @@ function registry-creds() {
 }
 
 function spotify-app-creds() {
+    os
     export SPOTIFY_ID=$(get-secret spotify id)
     export SPOTIFY_SECRET=$(get-secret spotify secret)
 }
